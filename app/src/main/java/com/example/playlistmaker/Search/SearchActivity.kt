dba82ca4ec3bf31a22abcb5d05b5855fea784b31
Ivan Titkov <ivan.titkov@aliexpress.ru>
2025-04-21 16:56:39 +0400

    add glide and icons

--- app/src/main/java/com/example/playlistmaker/Search/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/Search/SearchActivity.kt
@@ -1,10 +1,12 @@
 package com.example.playlistmaker.Search
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +18,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.MainActivity
 import com.example.playlistmaker.R
 import com.google.android.material.appbar.MaterialToolbar
@@ -90,17 +94,31 @@ class TrackViewHolder(item: View): RecyclerView.ViewHolder(item) {
     private val title: TextView
     private val subTitle: TextView
     private val image: ImageView
+    private val applicationContext: View
 
     init {
         title = item.findViewById<TextView>(R.id.track_cell_title)
         subTitle = item.findViewById<TextView>(R.id.track_cell_sub_title)
         image = item.findViewById<ImageView>(R.id.track_cell_image)
+        applicationContext = item
     }
 
     fun bind(model: Track) {
         title.text = model.artistName
         subTitle.text = model.trackName + " \\u2022 " +  model.trackTime
-        //сделать загрузку имаджа
+        Glide
+            .with(applicationContext)
+            .load(model.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .transform(RoundedCorners(dpToPx(2.toFloat(), applicationContext.context)))
+            .into(image)
+    }
+
+    fun dpToPx(dp: Float, context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics).toInt()
     }
 }
 
