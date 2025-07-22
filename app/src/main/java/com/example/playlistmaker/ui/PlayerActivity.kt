29b226cac75da34754d3e59f79dfb5b644ea9e70
Ivan Titkov <ivan.titkov@aliexpress.ru>
2025-07-22 18:35:34 +0400

    add binding too player

--- app/src/main/java/com/example/playlistmaker/ui/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/PlayerActivity.kt
@@ -7,38 +7,30 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.TypedValue
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityPlayerBinding
 import com.example.playlistmaker.domain.models.Track
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.gson.Gson
 import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
-
     private var mediaPlayer = MediaPlayer()
-    private lateinit var playImageView: ImageView
-    private lateinit var trackTimeTextView: TextView
     private var playerState = PlaybackState.Default
     private val handler = Handler(Looper.getMainLooper())
+    private lateinit var binding: ActivityPlayerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_player)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val toolbar = findViewById<MaterialToolbar>(R.id.playerToolbar)
         toolbar.setNavigationOnClickListener {
             finish()
-        }
-
-        playImageView = findViewById(R.id.icon_play)
-        playImageView.setOnClickListener {
-            playPause()
         }
 
         val extras = intent.extras
@@ -46,50 +38,38 @@ class PlayerActivity : AppCompatActivity() {
             val trackAsJson = extras.getString("Track")
             val track = Gson().fromJson(trackAsJson, Track::class.java)
 
-            val trackNameTextView = findViewById<TextView>(R.id.track_main_title)
-            trackNameTextView.text = track.trackName
+            binding.apply {
+                trackMainTitle.text = track.trackName
+                trackSubtitle.text = track.artistName
+                trackAlbumValue.text = track.collectionName
+                trackYearValue.text = SimpleDateFormat("yyyy", Locale.getDefault()).parse(track.releaseDate).toString()
+                trackCountryTitleValue.text = track.country
+                trackGanreValue.text = track.primaryGenreName
+                iconPlay.setOnClickListener {
+                    playPause()
+                }
+                val coverUrl = track.artworkUrl100.replaceAfterLast('/',getString(R.string.cover512))
+                Glide
+                    .with(applicationContext)
+                    .load(coverUrl)
+                    .placeholder(R.drawable.track_placeholder)
+                    .transform(RoundedCorners(dpToPx(8.toFloat())))
+                    .into(cover)
+            }
 
-            val trackArtistTextView = findViewById<TextView>(R.id.track_subtitle)
-            trackArtistTextView.text = track.artistName
-
-            trackTimeTextView = findViewById<TextView>(R.id.trackTimeCurrentValue)
             resetCurrentTime()
-            val trackAlumTextView = findViewById<TextView>(R.id.trackAlbumValue)
-            trackAlumTextView.text = track.collectionName
-
-            val trackYearTextView = findViewById<TextView>(R.id.trackYearValue)
-
-            val release = SimpleDateFormat("yyyy", Locale.getDefault()).parse(track.releaseDate)
-
-            trackYearTextView.text = release.toString()
-
-            val trackGanreTextView = findViewById<TextView>(R.id.trackGanreValue)
-            trackGanreTextView.text = track.primaryGenreName
-
-            val trackCountryTextView = findViewById<TextView>(R.id.trackCountryTitleValue)
-            trackCountryTextView.text = track.country
-
-            val cover = findViewById<ImageView>(R.id.cover)
-            val coverUrl = track.artworkUrl100.replaceAfterLast('/',getString(R.string.cover512))
-            Glide
-                .with(applicationContext)
-                .load(coverUrl)
-                .placeholder(R.drawable.track_placeholder)
-                .transform(RoundedCorners(dpToPx(8.toFloat())))
-                .into(cover)
-
             prepareMediaPlayer(track.previewUrl)
         }
     }
 
     private fun resetCurrentTime() {
-        trackTimeTextView.text = "00:00"
+        binding.trackTimeValue.text = "00:00"
     }
 
     private fun updateTrackTime() {
         handler.postDelayed( {
             val currentPosition = mediaPlayer.currentPosition
-            trackTimeTextView.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(currentPosition)
+            binding.trackTimeValue.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(currentPosition)
             updateTrackTime()
         },
             REFRESH_TIME_DELAY
@@ -109,7 +89,7 @@ class PlayerActivity : AppCompatActivity() {
             resetCurrentTime()
             mediaPlayer.seekTo(0)
             playerState = PlaybackState.Prepared
-            playImageView.setImageResource(R.drawable.play)
+            binding.iconPlay.setImageResource(R.drawable.play)
         }
     }
 
@@ -117,10 +97,12 @@ class PlayerActivity : AppCompatActivity() {
         updateTrackTime()
         mediaPlayer.start()
         playerState = PlaybackState.Playing
-        if (isDarkMode()) {
-            playImageView.setImageResource(R.drawable.pause_night)
-        } else {
-            playImageView.setImageResource(R.drawable.pause)
+        binding.apply {
+            if (isDarkMode()) {
+                iconPlay.setImageResource(R.drawable.pause_night)
+            } else {
+                iconPlay.setImageResource(R.drawable.pause)
+            }
         }
     }
 
@@ -128,10 +110,12 @@ class PlayerActivity : AppCompatActivity() {
         handler.removeCallbacksAndMessages(null)
         mediaPlayer.pause()
         playerState = PlaybackState.Paused
-        if (isDarkMode()) {
-            playImageView.setImageResource(R.drawable.play_night)
-        } else {
-            playImageView.setImageResource(R.drawable.play)
+        binding.apply {
+            if (isDarkMode()) {
+                iconPlay.setImageResource(R.drawable.play_night)
+            } else {
+                iconPlay.setImageResource(R.drawable.play)
+            }
         }
     }
 
