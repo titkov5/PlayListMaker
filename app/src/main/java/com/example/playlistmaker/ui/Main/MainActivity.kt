eea0a4dddaceb9bf5957835725ccb3bdb252591a
Ivan Titkov <ivan.titkov@aliexpress.ru>
2025-08-12 16:31:37 +0400

    add navigation bar

--- app/src/main/java/com/example/playlistmaker/ui/Main/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/Main/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.playlistmaker.ui.Main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMainBinding
-import com.example.playlistmaker.ui.Search.SearchFragment
-import com.example.playlistmaker.ui.Settings.SettingsFragment
-import com.example.playlistmaker.ui.media.MediaFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -16,16 +15,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        if (savedInstanceState==null) {
-            supportFragmentManager.commit {
-                this.add(R.id.rootFragmentContainerView,)
-            }
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
-//MediaFragment()
-//SettingsFragment()
-// SearchFragment()
